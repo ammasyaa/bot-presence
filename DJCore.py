1111f2159973d0abd3eec1862728a6a898ab0a1b
ammasyaa <70730816+ammasyaa@users.noreply.github.com>
2021-12-25 17:32:44 +0700

    Update DJCore.py

--- DJCore.py
+++ DJCore.py
@@ -15,7 +15,7 @@ async def _bot_protection(ctx):
 @bot.event
 async def on_ready():
     print(f'Bot is ready! Logged as in: {bot.user}')
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name=f"Youtube Music🎧"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=f"Red Dead Online"))
 
 if __name__ == "__main__":
     for extension in bot.initials:
